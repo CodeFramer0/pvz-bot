5f7a112b4aab2c993eea8b30951072ed67ff3ed5
CodeFramer <52332286+CodeFramer0@users.noreply.github.com>
2024-10-03 01:36:51 +0500

    remove template code

--- bot/handlers/errors/state_canceling.py
+++ bot/handlers/errors/state_canceling.py
@@ -4,27 +4,4 @@ from aiogram.dispatcher import FSMContext
 from loader import dp
 from states import *
 
-# @dp.message_handler(state=[])
-# @error_handler()
-# async def real_cancellation(message: types.Message, state:FSMContext = None):
-#     lang = await api.getUserLanguage(message.from_user.id)
-#     if message.text == '/restart':
-#         await state.finish()
-#         text = await api.getFuncText('cancellation_restart')
-#         return await message.answer(
-#             text = text[lang],
-#             reply_markup=bot_start_keyboard(lang))
 
-#     text = await api.getFuncText('cancellation_try')
-#     return await message.answer(text=text[lang])
-
-
-# @dp.message_handler(commands=['restart'])
-# @error_handler()
-# async def fake_cancellation(message: types.Message):
-#     lang = await api.getUserLanguage(message.from_user.id)
-#     text = await api.getFuncText('cancellation_restart')
-#     return await message.answer(
-#         text = text[lang],
-#         reply_markup=bot_start_keyboard(lang)
-#     )
