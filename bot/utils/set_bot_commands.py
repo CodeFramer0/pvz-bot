55495a8d79b2750eff847e788ffdf27875132d6d
CodeFramer <52332286+CodeFramer0@users.noreply.github.com>
2024-10-19 18:59:21 +0500

    create state canceling command (/cancel)

--- bot/utils/set_bot_commands.py
+++ bot/utils/set_bot_commands.py
@@ -5,5 +5,6 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Начальное меню"),
+            types.BotCommand("cancel", "Отменить создание заказа"),
         ]
     )
